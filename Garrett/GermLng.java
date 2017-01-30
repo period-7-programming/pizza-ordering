343a18cebe1752c8e61989a2e24403fe220de7ae
GarrettManley2 <jdjunk@gra.midco.net>
2017-01-30 14:21:39 -0600

    Added finished Pizza ordering

---
+++ Garrett/GermLng.java
@@ -0,0 +1,24 @@
+package pizza.ordering;
+
+class German extends Language {
+	{
+		q1 = "Hallo, welche Größe Pizza möchtest du?";
+		q2 = "Ok, was möchten Sie auf Ihrer Pizza?";
+		q3 = "Möchten Sie dünne oder dicke Kruste?";
+		q4 = "Wie heißen Sie?";
+		s1 = "Sie haben eine Pizza, die nicht existiert so danke für Brechen, Wissenschaft bestellt.";
+		r1 = "Sie haben eine ";
+		r2 = " pizza mit ";
+		r3 = " toppings auf einem ";
+		s2 = " _    _ _                             _            __ _   ";
+		s3 = "| |  | (_)                           | |          / _| |";
+		s4 = "| |  | |_ ___ ___  ___ _ __  ___  ___| |__   __ _| |_| |_ ";
+		s5 = "| |/\\| | / __/ __|/ _ \\ '_ \\/ __|/ __| '_ \\ / _` |  _| __|";
+		s6 = "\\  /\\  / \\__ \\__ \\  __/ | | \\__ \\ (__| | | | (_| | | | |_";
+		s7 = " \\/  \\/|_|___/___/\\___|_| |_|___/\\___|_| |_|\\__,_|_|  \\__|";
+	}
+
+	void run() {
+		super.run();
+	}
+}
--- Garrett/Main.java
+++ Garrett/Main.java
@@ -1,92 +1,22 @@
 package pizza.ordering;
 
 import java.util.Scanner;
-import java.util.List;
-import java.util.ArrayList;
 
 public class Main {
-	static List<Object> a = new ArrayList<Object>();
 
 	public static void main(String[] args) {
 		Scanner i = new Scanner(System.in);
-
-		boolean loop = true;
-		boolean lang = false; // false = English, true = German
 
 		System.out.println("English, Deutsch");
 		String g = i.nextLine();
 
 		if (g.equalsIgnoreCase("english")) {
-			lang = false;
-		} else {
-			lang = true;
+			Order o = new Order();
+			o.run();
+		} else if (g.equalsIgnoreCase("deutsch")) {
+			Order o = new GrmOrd();
+			o.run();
 		}
-
-		while (loop) {
-
-			if (!lang) {
-				System.out.println("To order a pizza type, English. To display all orders type, Display english");
-				g = i.nextLine();
-				if (g.equalsIgnoreCase("display")) {
-					if (a.size() == 0) {
-						System.out.println("You haven't ordered anything yet");
-					} else {
-						System.out.println("Displaying orders");
-						System.out.println();
-						for (int f = 0; f < a.size(); f++) {
-							English r = (English) a.get(f);
-							System.out.println("Order " + (f + 1));
-							System.out.println("Size " + r.size);
-							System.out.println("Toppings " + r.toppings);
-							System.out.println("Crust " + r.thickness);
-							System.out.println("");
-						}
-					}
-				} else if (g.equalsIgnoreCase("english")) {
-					a.add(new English());
-					English b = (English) a.get(a.size() - 1);
-					b.run();
-				} else {
-					System.out.println("Invalid command");
-				}
-
-			}
-
-			if (lang) {
-				System.out
-						.println("Einen Pizzattyp bestellen, deutsch. Um alle Aufträge anzuzeigen, wählen Sie Deutsch");
-				g = i.nextLine();
-				if (g.equalsIgnoreCase("display")) {
-					if (a.size() == 0){
-						System.out.println("Sie haben noch nichts bestellt");
-					} else {
-					System.out.println("Aufträge anzeigen");
-					System.out.println();
-					for (int f = 0; f < a.size(); f++) {
-						German r = (German) a.get(f);
-						System.out.println("Order " + (f + 1));
-						System.out.println("Größe " + r.size);
-						System.out.println("Belägen " + r.toppings);
-						System.out.println("Kruste " + r.thickness);
-						System.out.println("");
-					}
-				}
-				} else if (g.equalsIgnoreCase("deustch")) {
-					a.add(new German());
-					German b = (German) a.get(a.size() - 1);
-					b.run();
-				} else {
-					System.out.println("Ungültiger Befehl");
-				}
-			}
-
-			if (g.equalsIgnoreCase("exit") || g.equalsIgnoreCase("ausfahrt")) {
-				loop = false;
-			}
-			g = null;
-
-		}
-
 		i.close();
-
-	}}
+	}
+}
